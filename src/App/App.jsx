d6c52ebaca207ec1d061deccd0e3405819e4cea4
fabien.rivalain <fabien.rivalain@cgi.com>
2022-05-03 14:47:24 +0200

    props onclick

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,11 +6,16 @@ function App() {
   return (
     <div className="App">
       {/* <Button text="Un texte de button"></Button> */}
-      <Button bgColor="blue" color="black"></Button>
+      <Button 
+        bgColor="blue" 
+        color="black" 
+        onButtonClicked={()=>{console.log('test onButtonClicked')}}
+      >
+      </Button>
       <Button>
         <div>text children node</div>
       </Button>
-      <Button bgColor="aquamarine" color="red">
+      <Button bgColor="aquamarine" color="red" style={{textDecoration:"underline", fontWeight:900}}> 
         <div>text children node1</div>
         <div>text children node2</div>
       </Button>
--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -4,18 +4,33 @@ import PropTypes from 'prop-types';
 
 const Button = (props)=>{
     console.log(props)
-    return <button className={style.Button} style={ {backgroundColor:props.bgColor, color:props.color} }>{props.children}</button>
+    return (
+        <button
+            className={style.Button}
+            style={ {...props.style, backgroundColor:props.bgColor, color:props.color} }
+            type={props.type}
+            onClick={(evt)=>{
+                props.onButtonClicked();
+            }}
+        >
+            {props.children}
+        </button>
+    );
 }
 
 Button.propTypes={
     children: PropTypes.any.isRequired,
     bgColor: PropTypes.string.isRequired,
-    color : PropTypes.string.isRequired
+    color : PropTypes.string.isRequired,
+    style : PropTypes.object,
+    onButtonClicked : PropTypes.func.isRequired
 }
 
 Button.defaultProps={
     bgColor : 'red',
     children : 'defaultProps children',
-    color : 'white'
+    color : 'white',
+    type: 'button',
+    onButtonClicked : ()=>{}
 }
 export default Button
