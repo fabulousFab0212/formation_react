b9d9bb601393622131cb3914b5c9d3137d6c2338
fabien.rivalain <fabien.rivalain@cgi.com>
2022-05-03 15:08:11 +0200

    new App avec React.Component + componentDidUpdate

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-import "./App.css";
-import Button from "./components/ui/Button/Button";
+import React from 'react';
+import Button from './components/ui/Button/Button';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Button text="Un texte de button"></Button> */}
-      <Button 
-        bgColor="blue" 
-        color="black" 
-        onButtonClicked={()=>{console.log('test onButtonClicked')}}
-      >
-      </Button>
-      <Button>
-        <div>text children node</div>
-      </Button>
-      <Button bgColor="aquamarine" color="red" style={{textDecoration:"underline", fontWeight:900}}> 
-        <div>text children node1</div>
-        <div>text children node2</div>
-      </Button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={counter:1, title:'Mon Titre'}
+  }
+
+  componentDidUpdate(oldProps,oldState){
+    console.log("component update : ", oldState, this.state)
+  }
+
+  render() {
+    return (
+      <div className='App'>
+          Valeur du counter : {this.state.counter}
+          <hr/>
+          
+          <Button
+            bgColor="blue"
+            onButtonClicked={
+              ()=>{
+                this.setState({counter:this.state.counter+1});
+              }
+            }
+          >
+            Ajouter 1
+          </Button>
+          <hr/>
+
+          <Button
+            onButtonClicked={
+              ()=>{
+                this.setState({counter:this.state.counter-1});
+              }
+            }
+          >
+            Enlever 1
+          </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
