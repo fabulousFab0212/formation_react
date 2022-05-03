ed3852bdf5f9b7b85710f20652dfa1a8e8d23678
fabien.rivalain <fabien.rivalain@cgi.com>
2022-05-03 14:28:14 +0200

    gestion propTypes 2

--- src/App/components/ui/Button/Button.js
+++ src/App/components/ui/Button/Button.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import style from './Button.module.css'
+import PropTypes from 'prop-types';
 
 const Button = (props)=>{
     console.log(props)
-    return <button className={style.Button}>{props.children}</button>
+    return <button className={style.Button} style={ {backgroundColor:props.bgColor, color:props.color} }>{props.children}</button>
 }
 
+Button.propTypes={
+    children: PropTypes.any.isRequired,
+    bgColor: PropTypes.string.isRequired,
+    color : PropTypes.string.isRequired
+}
+
+Button.defaultProps={
+    bgColor : 'red',
+    children : 'defaultProps children',
+    color : 'white'
+}
 export default Button
