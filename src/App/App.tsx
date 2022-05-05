487467f2f75dce873c6c7ddedf50b29ab96dd26c
fabien.rivalain <fabien.rivalain@cgi.com>
2022-05-05 12:28:30 +0200

    gestion router react-router react-router-dom

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,19 +6,37 @@ import  { ConnectedMemeForm } from './components/ui/MemeForm/MemeForm';
 import  {ConnectdedMemeThumbnail } from './components/ui/MemeThumbnail/MemeThumbnail';
 import ConnectedMemeViewer from './components/ui/ConnectedMemeViewer/ConnectedMemeViewer';
 import NavBar from './components/ui/NavBar/NavBar';
+import {Route, Routes, useParams} from 'react-router-dom'
+import {useEffect} from 'react';
+import { ACTIONS_CURRENT, store } from './store/store';
+
 
 const App = () => {
     return (
       <div className='App'>
           <NavBar/>
-          <ConnectdedMemeThumbnail/>
-          <FlexW>
-              <ConnectedMemeViewer/>
-              <ConnectedMemeForm/>
-          </FlexW>
-          
+          <Routes>
+            <Route path="/" element={<div>Hello a tous</div>}/>
+            <Route path="/editor" element={<MemeEditor/>}/>
+            <Route path="/editor/:id" element={<MemeEditor/>}/>
+            <Route path="/thumbnail" element={<ConnectdedMemeThumbnail/>}/>
+          </Routes>
       </div>
     );
 };
+
+function MemeEditor(){
+  /*const id = useParams().id;
+  useEffect(() => {
+    store.dispatch({type:ACTIONS_CURRENT.UPDATE_MEME, value:store.getState().ressources.memes})
+  })*/
+  console.log(useParams());
+  return (
+    <FlexW>
+      <ConnectedMemeViewer/>
+      <ConnectedMemeForm/>
+  </FlexW>
+  );
+}
 
 export default App;
--- src/App/components/ui/NavBar/NavBar.js
+++ src/App/components/ui/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./NavBar.module.css";
 import {Container, Nav, Navbar} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import { LinkContainer } from "react-router-bootstrap";
 
 
 const NavBar = (props) => {
@@ -10,13 +11,14 @@ const NavBar = (props) => {
     <div className={style.NavBar} data-testid="NavBar">
       <Navbar bg="primary" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Meme Generator</Navbar.Brand>
+        <LinkContainer to="/"><Navbar.Brand>Meme Generator</Navbar.Brand></LinkContainer>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#new">New</Nav.Link>
-          <Nav.Link href="#edit">Edit</Nav.Link>
-          <Nav.Link href="#thumbnail">Thumbnail</Nav.Link>
-          <Nav.Link href="#thumbnail-pdf">Thumbnail PDF</Nav.Link>
+          <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+          <LinkContainer to="/new"><Nav.Link>New</Nav.Link></LinkContainer>
+          <LinkContainer to="/editor"><Nav.Link>Edit</Nav.Link></LinkContainer>
+          <LinkContainer to="/editor/1"><Nav.Link>Edit/1</Nav.Link></LinkContainer>
+          <LinkContainer to="/thumbnail"><Nav.Link>Thumbnail</Nav.Link></LinkContainer>
+          <LinkContainer to="/thumbnailPDF"><Nav.Link>ThumbnailPDF</Nav.Link></LinkContainer>
         </Nav>
         </Container>
       </Navbar>
